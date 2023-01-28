ac8f1c2059b4c79f5279c207dc44f54afc7e688f
shanepatra <shanepatra@gmail.com>
2023-01-28 18:19:51 -0500

    Modified the home page

--- src/App.js
+++ src/App.js
@@ -4,25 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
-      <link rel="stylesheet" href="style.css">
-        <head>
-   
-        </head>
-      <body>
-        <img src="riseup.png" width=10%>
-        <h1>RiseUp</h1>
-        <img src="img.png" width=10%>
-        <form action="/action_page.php">
-            <label for="fname">First name:</label>
-            <input type="text" id="fname" name="fname"><br><br>
-            <label for="lname">Last name:</label>
-            <input type="text" id="lname" name="lname"><br><br>
-            <label for="lname">Birth date:</label>
-            <input type="date" id="bday" name="bday"><br><br>
-            <input type="submit" value="Submit">
-        </form>
-      </body>
+      {/* <img src="riseup.png" width=10% \> */}
+      <h1>RiseUp</h1>
+      {/* <img src="img.png" width=10%> */}
+
+      <label for="fname">First name:</label>
+      <input type="text" id="fname" name="fname"/><br/>
+      <label for="lname">Last name:</label>
+      <input type="text" id="lname" name="lname"/><br/>
+      <label for="lname">Birth date:</label>
+      <input type="date" id="bday" name="bday"/><br/>
+      <input type="submit" value="Submit"/>
+
 
     </div>
   );
