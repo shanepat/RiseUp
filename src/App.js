116569cd5da14fcbaa4c27e52b3c2c016d2fef6c
Fabian Gabriel <121213635+gabrielf598@users.noreply.github.com>
2023-01-28 18:13:22 -0500

    Adding home page

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Now for SpartaHack 8!
-          <br/>
-          <br/>
-        </a>
-        <button>Start Here!</button>
-      </header>
+      
+      <link rel="stylesheet" href="style.css">
+        <head>
+   
+        </head>
+      <body>
+        <img src="riseup.png" width=10%>
+        <h1>RiseUp</h1>
+        <img src="img.png" width=10%>
+        <form action="/action_page.php">
+            <label for="fname">First name:</label>
+            <input type="text" id="fname" name="fname"><br><br>
+            <label for="lname">Last name:</label>
+            <input type="text" id="lname" name="lname"><br><br>
+            <label for="lname">Birth date:</label>
+            <input type="date" id="bday" name="bday"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+      </body>
+
     </div>
   );
 }
